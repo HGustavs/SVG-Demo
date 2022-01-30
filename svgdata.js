45530c4d86d0434b7af1d905a8972381ce9a74ce
Henrik Gustavsson <HGustavs@users.noreply.github.com>
2022-01-30 01:39:34 +0100

    Seed
    Seed of index files

---
+++ svgdata.js
@@ -0,0 +1,16 @@
+var svgdata=[
+  {
+      name:"First Example",
+      content:`
+ <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
+      <line x1="0" y1="80" x2="100" y2="20" stroke="black" />
+      <circle cx="100" cy="100" r="50" fill="green" />
+      <polygon points="20,20 180,50 100,200" stroke="red" fill="none" />
+      <text x="100" y="100" font-size="30" text-anchor="middle" fill="white">SVG</text>
+  </svg> 
+`
+  }
+
+
+
+]
