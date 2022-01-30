5a6cfc48cc2bc876abf8e8dd1698198c7b33c8df
Henrik Gustavsson <HGustavs@users.noreply.github.com>
2022-01-30 19:13:01 +0100

    Style setting
    Style setting attempt

--- svgdata.js
+++ svgdata.js
@@ -6,12 +6,28 @@ var svgdata=[
   <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
     <line x1="0" y1="80" x2="100" y2="20" stroke="black" />
     <circle cx="100" cy="100" r="50" fill="green" />
-    <polygon points="20,20 180,50 100,200" stroke="red" fill="none" />
+    <polygon id="tri" points="20,20 180,50 100,200" stroke="red" fill="none" />
     <text x="100" y="100" font-size="30" text-anchor="middle" fill="white">SVG</text>
   </svg> 
   `,
   style:`
-    Hello!
+  @keyframes snurr { 
+    0% { 
+        transform: translate(135px,100px) scale(0.3) rotate(0deg); 
+    } 
+    100%
+     { 
+        transform: translate(135px,100px) scale(0.3) rotate(360deg); 
+    }
+    
+}
+
+#tri{                
+    animation-name: snurr;
+    animation-duration: 4000ms;
+    animation-iteration-count: infinite;
+    animation-timing-function: linear;
+}
   `
   },
   {
