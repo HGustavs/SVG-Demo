b6c4c7183bf0cb2628df8f4f52d2e1c54593ccca
Henrik Gustavsson <HGustavs@users.noreply.github.com>
2022-01-30 17:42:55 +0100

    Dropdown file support

--- svgdata.js
+++ svgdata.js
@@ -1,6 +1,7 @@
 var svgdata=[
   {
-      name:"First Example",
+      name:"SVG Basics",
+      kind:0,
       content:`
  <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
       <line x1="0" y1="80" x2="100" y2="20" stroke="black" />
@@ -8,8 +9,32 @@ var svgdata=[
       <polygon points="20,20 180,50 100,200" stroke="red" fill="none" />
       <text x="100" y="100" font-size="30" text-anchor="middle" fill="white">SVG</text>
   </svg> 
-`
-  }
+`,
+  },
+  {
+    name:"SVG Advanced",
+    kind:0,
+    content:`
+<svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
+    <line x1="0" y1="80" x2="100" y2="20" stroke="black" />
+    <circle cx="100" cy="100" r="50" fill="green" />
+    <polygon points="20,20 180,50 100,200" stroke="red" fill="none" />
+    <text x="100" y="100" font-size="30" text-anchor="middle" fill="white">SVG</text>
+</svg> 
+`,
+},
+{
+  name:"Css Example",
+  kind:0,
+  content:`
+<svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
+  <line x1="0" y1="80" x2="100" y2="20" stroke="black" />
+  <circle cx="100" cy="100" r="50" fill="green" />
+  <polygon points="20,20 180,50 100,200" stroke="red" fill="none" />
+  <text x="100" y="100" font-size="30" text-anchor="middle" fill="white">SVG</text>
+</svg> 
+`,
+}
 
 
 
